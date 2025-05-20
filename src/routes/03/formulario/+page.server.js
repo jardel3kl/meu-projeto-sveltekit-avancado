e78253e36671c7726b05f7ef2fe2697d75ff1629
jardel3kl <jardel3kl@gmail.com>
2025-05-20 17:00:07 -0400

    top

---
+++ src/routes/03/formulario/+page.server.js
@@ -0,0 +1,6 @@
+export async function load({url}) {
+    const fruta = url.searchParams.get('fruta');
+    const legume = url.searchParams.get('legume');
+
+    return {fruta, legume}
+}
---
+++ src/routes/03/formulario/+page.svelte
@@ -0,0 +1,16 @@
+
+<script>
+    let { data } = $props();
+</script>
+
+<form>
+    <input type="text" name="fruta" placeholder="Qual tua fruta preferida?" />
+    <input type="text" name="legume" placeholder="E legume?" />
+    <button type="submit">buscar</button>
+</form>
+
+{#if data.fruta && data.legume}
+Você gosta da fruta {data.fruta} e do Legume {data.legume}
+{:else}
+Informe uma fruta e um legume...
+{/if}
