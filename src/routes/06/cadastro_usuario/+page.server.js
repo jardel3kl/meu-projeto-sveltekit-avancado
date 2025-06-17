ee17fa9850242ddd310161e95588c425283990d7
jardel3kl <jardel3kl@gmail.com>
2025-06-17 15:50:44 -0400

    top

--- src/routes/06/cadastro_usuario/+page.server.js
+++ src/routes/06/cadastro_usuario/+page.server.js
@@ -1,16 +1,22 @@
-import { fail } from "@sveltejs/kit";
+import {fail} from '@sveltejs/kit';
 
 export const actions = {
     default: async ({ request }) => {
         const data = await request.formData();
-        const dados{
-            nome: data.get('nome')
-            email: data.get('email')
-            senha: data.get('senha')
-            confirmacaosenha: data.get('confirmacaosenha')
-            nascimento: data.get('nascimento')
-
-
+        const dados = {
+            nome: data.get('nome'),
+            email: data.get('email'),
+            senha: data.get('senha'),
+            confirmacaosenha: data.get('confirmacaosenha'),
+            nascimento: data.get('nascimento'),
         }
-    }
-}
+
+        if (dados.nome.length < 2)
+        return fail(400,{erro: 'O nome deve ter pelo menos dois caracteres!', dados })
+    if (!dados.email.includes('@'))
+    return fail(400, { erro: 'E-mail inválido!', dados })
+if (dados.senha.length < 4)
+return fail(400, { erro: 'A senha deve ter pelo menos quatro caracteres', dados })
+return {sucesso: true, nome: dados.nome }
+        }
+    };
---
+++ src/routes/06/cadastro_usuario/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    let {form} = $props();
+</script>
+
+<form method="POST">
+    <input name="nome" type="text" placeholder="Nome do usuário" value={form?.dados.nome || ''}/>
+    <input name="email" type="email" placeholder="E-mail do usuário" value={form?.dados.email || ''}/>
+    <input name="senha" type="password" placeholder="Senha do usuário" value={form?.dados.senha || ''}/>
+    <input name="confirmacaosenha" type="password" placeholder="Confirmação de senha" value={form?.dados.confirmacaosenha || ''}/>
+    <input name="nascimento" type="date" value={form?.dados.nascimento || ''}/>
+<button>Cadastrar</button>
+</form>
+
+{#if !form}
+<p>Preencha o formulário!</p>
+{:else if form.erro}
+<p style="color: red;"><strong>Erro:</strong> {form.erro}</p>
+{:else if form.sucesso}
+<p style="color: green;"><strong>Sucesso;</strong> Usuário {form?.nome} cadastrado!</p>
+    {/if}
+    
